ad77b69399b8945ce5e971a958791df2f19c4941
Barua98 <ronyb98@gmail.com>
2023-05-31 15:37:44 +0200

    update

--- src/App.js
+++ src/App.js
@@ -6,16 +6,13 @@ function App() {
     <div className="App">
       <h1>new Header</h1>
       <p>New article</p>
-<<<<<<< HEAD
       <image>New image</image>
-=======
       <h3>Under overskrift</h3>
       <image>New image</image>
       <p>New article</p>
       <h1>Newer header</h1>
       <p>Henriette er her</p>
       <p>heihei</p>
->>>>>>> 3ac6131c9a111fa5f361766a32e0a9819b1f9eeb
     </div>
   );
 }
