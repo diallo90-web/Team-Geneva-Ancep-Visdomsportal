43edfa5b1951dfd80f4e6c00f58eb95c440d61e3
Barua98 <ronyb98@gmail.com>
2023-06-01 16:03:31 +0200

    lagre endringer

---
+++ src/components/shared/bigbutton/BigBtn.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./BigBtn.css"
+
+const BigBtn = () => {
+    return (
+        <button className="big-btn">Big Button</button>
+    );
+};
+
+export default BigBtn;
