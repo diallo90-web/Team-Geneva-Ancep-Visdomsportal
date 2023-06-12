22901fced26c1d95fa0f4e5651e1405e7c65f6ee
sjuriff <sjursand@gmail.com>
2023-06-12 21:20:01 +0200

    media queries on navigation component

--- src/pages/navigationpage/NavigationPage.js
+++ src/pages/navigationpage/NavigationPage.js
@@ -18,7 +18,7 @@ console.log(childData)
 
     return (
         <div className={"navigation-page-wrapper"}>
-            <CardTwo  title={childData? "":"Naviger & Utforsk"} headingLevel={mobileScreen? "h3":"h1"} description={childData? " ": "Velkommen til navigasjonssiden. Her kan du navigere deg rundt i Arne Næss sin verden. Trykk på sirkelen for å komme i gang!" }/>
+            <CardTwo  title={childData? "":"Naviger & Utforsk"} headingLevel={mobileScreen? "h3":"h1"} description={childData? " ": mobileScreen? "Trykk på sirkelen for å komme i gang!" :"Velkommen til navigasjonssiden. Her kan du navigere deg rundt i Arne Næss sin verden. Trykk på sirkelen for å komme i gang!" }/>
             <div className="component-wrapper">
             <Navigation func ={pull_data}/>
             </div>
