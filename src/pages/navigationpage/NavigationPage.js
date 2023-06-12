ed1c9423fbaca339f5c7f93a2c530a8cad518652
sjuriff <sjursand@gmail.com>
2023-06-12 21:10:09 +0200

    media queries on navigation component

--- src/pages/navigationpage/NavigationPage.js
+++ src/pages/navigationpage/NavigationPage.js
@@ -2,6 +2,7 @@ import Navigation from "../../components/navigation/Navigation"
 import CardTwo from "../../components/cardtwo/CardTwo"
 import "./NavigationPage.css"
 import { useState } from "react"
+import { useMediaQuery } from "react-responsive";
 
 const NavigationPage = () => {
 
@@ -11,13 +12,13 @@ const NavigationPage = () => {
         setChildData(data)
     }
 
-   
+    const mobileScreen = useMediaQuery({query: '(max-width: 480px)'})
 
 console.log(childData)
 
     return (
-        <div className="navigation-page-wrapper">
-            <CardTwo  title={"Navigasjon"} headingLevel={"h1"} description={childData? " ": "Velkommen til navigasjonssiden, her kan du navigere deg rundt i Arne Næss sin verden. Trykk på sirkelen for å komme i gang!" }/>
+        <div className={"navigation-page-wrapper"}>
+            <CardTwo  title={childData? "":"Naviger & Utforsk"} headingLevel={mobileScreen? "h3":"h1"} description={childData? " ": "Velkommen til navigasjonssiden. Her kan du navigere deg rundt i Arne Næss sin verden. Trykk på sirkelen for å komme i gang!" }/>
             <div className="component-wrapper">
             <Navigation func ={pull_data}/>
             </div>
