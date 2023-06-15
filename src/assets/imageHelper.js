cee816a41cf71e4f110e2861f226da1bbc5053fc
sjuriff <sjursand@gmail.com>
2023-06-15 19:42:44 +0200

    cleanup assets

--- src/assets/imageHelper.js
+++ src/assets/imageHelper.js
@@ -17,6 +17,221 @@ import bookImageEight from "../images/book-eight.png"
 import bookImageNine from "../images/book_nine.png"
 import bookImageTen from "../images/book_ten.png"
 
+import myImage from "../images/arne1.jpg";
+import myImage2 from "../images/arne2.jpg";
+import myImage3 from "../images/arne3.jpg";
+import myImage4 from "../images/arne4.jpg";
+import myImage5 from "../images/arne5.jpg";
+import myImage6 from "../images/arne6.jpg";
+import myImage7 from "../images/arne7.jpg";
+import myImage8 from "../images/arne8.jpg";
+import myImage9 from "../images/arne9.jpg";
+import myImage10 from "../images/arne10.jpg";
+import myImage11 from "../images/arne11.jpg";
+import myImage12 from "../images/arne12.jpg";
+import myImage13 from "../images/arne13.jpg";
+import myImage14 from "../images/arne14.jpg";
+import myImage15 from "../images/arne15.jpg";
+import myImage16 from "../images/arne16.jpg";
+import myImage17 from "../images/arne17.jpg";
+import myImage18 from "../images/arne18.jpg";
+import myImage19 from "../images/arne19.jpg";
+import myImage20 from "../images/arne20.jpg";
+import myImage21 from "../images/arne21.jpg";
+import myImage22 from "../images/arne22.jpg";
+import myImage23 from "../images/arne23.jpg";
+import myImage24 from "../images/arne24.jpg";
+import myImage25 from "../images/arne25.jpg";
+import myImage26 from "../images/arne26.jpg";
+import myImage27 from "../images/arne27.jpg";
+import myImage28 from "../images/arne28.jpg";
+import myImage29 from "../images/arne29.jpg";
+import myImage30 from "../images/arne30.jpg";
+import myImage31 from "../images/arne31.jpg";
+import myImage32 from "../images/arne32.jpg";
+import myImage33 from "../images/arne33.jpg";
+import myImage34 from "../images/arne34.jpg";
+import myImage35 from "../images/arne35.jpg";
+import myImage36 from "../images/arne36.jpg";
+import myImage37 from "../images/arne37.jpg";
+import myImage38 from "../images/arne38.jpg";
+import myImage39 from "../images/arne39.jpg";
+import myImage40 from "../images/arne40.jpg";
+
+
+
+export const galleryImages = () =>{
+    const images = [
+        {
+            id: 1,
+            image: myImage
+        },
+        {
+            id: 2,
+            image: myImage2
+        },
+        {
+            id: 3,
+            image: myImage3
+        },
+        {
+            id: 4,
+            image: myImage4
+        },
+        {
+            id: 5,
+            image: myImage5
+        },
+        {
+            id: 6,
+            image: myImage6
+        },
+        {
+            id: 7,
+            image: myImage7
+        },
+        {
+            id: 8,
+            image: myImage8
+        },
+        {
+            id: 9,
+            image: myImage9
+        },
+        {
+            id: 10,
+            image: myImage10
+        },
+        {
+            id: 11,
+            image: myImage11
+        },
+        {
+            id: 12,
+            image: myImage12
+        },
+        {
+            id: 13,
+            image: myImage13
+        },
+        {
+            id: 13,
+            image: myImage14
+        },
+        {
+            id: 14,
+            image: myImage14
+        },
+        {
+            id: 15,
+            image: myImage15
+        },
+        {
+            id: 16,
+            image: myImage16
+        },
+        {
+            id: 17,
+            image: myImage17
+        },
+        {
+            id: 18,
+            image: myImage18
+        },
+        {
+            id: 19,
+            image: myImage19
+        },
+        {
+            id: 20,
+            image: myImage20
+        },
+        {
+            id: 21,
+            image: myImage21
+        },
+        {
+            id: 22,
+            image: myImage22
+        },
+        {
+            id: 23,
+            image: myImage23
+        },
+        {
+            id: 24,
+            image: myImage24
+        },
+        {
+            id: 25,
+            image: myImage25
+        },
+        {
+            id: 26,
+            image: myImage26
+        },
+        {
+            id: 27,
+            image: myImage27
+        },
+        {
+            id: 28,
+            image: myImage28
+        },
+        {
+            id: 29,
+            image: myImage29
+        },
+        {
+            id: 30,
+            image: myImage30
+        },
+        {
+            id: 31,
+            image: myImage31
+        },
+        {
+            id: 32,
+            image: myImage32
+        },
+        {
+            id: 33,
+            image: myImage33
+        },
+        {
+            id: 34,
+            image: myImage34
+        },
+        {
+            id: 35,
+            image: myImage35
+        },
+        {
+            id: 36,
+            image: myImage36
+        },
+        {
+            id: 37,
+            image: myImage37
+        },
+        {
+            id: 38,
+            image: myImage38
+        },
+        {
+            id: 39,
+            image: myImage39
+        },
+        {
+            id: 40,
+            image: myImage40
+        },
+    ]
+
+    return images;
+}
+
+
 export const workImages = () =>{
     const images = [
         { 
