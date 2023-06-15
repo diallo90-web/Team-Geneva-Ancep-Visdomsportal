d619a9f28be183bf9acb7b1b6e142dcceacd9621
sjuriff <sjursand@gmail.com>
2023-06-15 12:42:08 +0200

    json file works and pictures works

---
+++ src/assets/imageHelper.js
@@ -0,0 +1,38 @@
+
+import imageOne from "../images/Potter.jpeg"
+import imageTwo from "../images/Shallow.jpeg"
+import imageThree from "../images/deep_plattform.jpeg"
+import imageFour from "../images/bevegelsen.jpeg"
+import imageFive from "../images/poor.jpeg"
+import imageSix from "../images/basics.jpeg"
+
+export const workImages = () =>{
+    const images = [
+        { 
+            id: 1,
+            image: imageOne
+        },
+        { 
+            id: 2,
+            image: imageTwo
+        },
+        { 
+            id: 3,
+            image: imageThree
+        },
+        { 
+            id: 4,
+            image: imageFour
+        },
+        { 
+            id: 5,
+            image: imageFive
+        },
+        { 
+            id: 6,
+            image: imageSix
+        }
+    ]
+
+    return images
+}
